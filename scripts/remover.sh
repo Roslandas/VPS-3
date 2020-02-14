aefb5c44d31537c790fea694e9bfbd522a3299b3
rolka1978 <59371841+rolka1978@users.noreply.github.com>
2020-02-14 20:47:47 +0200

    Update remover.sh

--- scripts/remover.sh
+++ scripts/remover.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%32s%s%-13s\n' "Remover Usuário SSH" ; tput sgr0
+tput setaf 7 ; tput setab 4 ; tput bold ; printf '%32s%s%-13s\n' "Pasalinti vartotoja" ; tput sgr0
 echo ""
-tput bold ; echo "Lista de usuários:" ; echo "" ; tput sgr0
+tput bold ; echo "Vartotoju sąrasas:" ; echo "" ; tput sgr0
 tput setaf 3 ; tput bold ; awk -F : '$3 >= 500 { print $1 }' /etc/passwd | grep -v '^nobody' ; tput sgr0
 echo ""
-read -p "Nombre de usuário para remover: " user
+read -p "Pasalinamas vartotojo vardas: " user
 if [[ -z $user ]]
 then
-	tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "usted digito un usuario no valido" ; echo "" ; tput sgr0
+	tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "galiojantis vartotojas" ; echo "" ; tput sgr0
 	exit 1
 else
 	if [[ `grep -c /$user: /etc/passwd` -ne 0 ]]
@@ -16,18 +16,18 @@ else
 		if [[ `grep -c $user /tmp/rem` -eq 0 ]]
 		then
 			deluser $user > /dev/null
-			tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "el usuário $user fue removido!" ; echo "" ; tput sgr0
+			tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "vartotojas $ vartotojas buvo pasalintas!" ; echo "" ; tput sgr0
 			grep -v ^$user[[:space:]] /root/usuarios.db > /tmp/ph ; cat /tmp/ph > /root/usuarios.db
 			exit 1
 		else
-			tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Usuário conectado. Desconectando..." ; tput sgr0
+			tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Vartotojas prisijunge. Atjungimas..." ; tput sgr0
 			pkill -f "$user"
 			deluser $user > /dev/null
-			tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "elusuário $user fue removido con exito!" ; echo "" ; tput sgr0
+			tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "vartotojas $ user buvo sekmingai pasalintas!" ; echo "" ; tput sgr0
 			grep -v ^$user[[:space:]] /root/usuarios.db > /tmp/ph ; cat /tmp/ph > /root/usuarios.db
 			exit 1
 		fi
 	else
-		tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "el usuário $user no existe!" ; echo "" ; tput sgr0
+		tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "vartotojas $ vartotojas neegzistuoja!" ; echo "" ; tput sgr0
 	fi
 fi
