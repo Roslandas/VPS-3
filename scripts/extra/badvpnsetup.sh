79a7a2e69ac2566d64647bf02c29d6c9b04f0530
rolka1978 <59371841+rolka1978@users.noreply.github.com>
2020-02-16 17:58:16 +0200

    Update badvpnsetup.sh

--- scripts/extra/badvpnsetup.sh
+++ scripts/extra/badvpnsetup.sh
@@ -16,18 +16,18 @@ killall badvpn-udpgw
 fi" > /bin/badvpn
 chmod +x /bin/badvpn
 if [ -f /bin/badvpn-udpgw ]; then
-echo -e "\033[1;32mBadvpn ya esta instalado\033[0m"
+echo -e "\033[1;32mBadvpn yra idiegta\033[0m"
 rm -rf badvpnsetup.sh >/dev/null 2>/dev/null
 exit
 else
 clear
 fi
-echo -e "\033[1;31m           Instalador Badvpn\n\033[0m"
+echo -e "\033[1;31m           Diegiamas Badvpn\n\033[0m"
 echo -e "Haciendo la download de Badvpn"
 wget -O /bin/badvpn-udpgw https://raw.githubusercontent.com/rolka1978/VPS-3/master/scripts/extra/badvpn-udpgw -o /dev/null
 chmod +x /bin/badvpn-udpgw
 clear
-echo -e "\033[1;32m             Instalacion completa\033[0m" 
-echo -e "\033[1;37mComandos:\n\033[1;31mbadvpn start\033[1;37m para iniciar o badvpn"
-echo -e "\033[1;31mbadvpn stop \033[1;37m para parar o badvpn\033[0m"
+echo -e "\033[1;32m             Diegimas baigtas\033[0m" 
+echo -e "\033[1;37mKomandos:\n\033[1;31mbadvpn start\033[1;37m pradeti badvpn"
+echo -e "\033[1;31mbadvpn stop \033[1;37m sustabdyti badvpn\033[0m"
 rm -rf badvpnsetup.sh >/dev/null 2>/dev/null
